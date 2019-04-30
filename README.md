8d09075adb268cff078703bbc2be016a3b882326
leo <leok90@gmail.com>
2019-04-30 22:41:14 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Dublin Forecast
+
+Scrape tomorrow's weather forecast for the Dublin area from Met Éireann's website.
+
+Send it to a different AWS Lambda endpoint. In my case. I have one set up that
+will text something to me via a Telegram bot.
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import os
+import requests
+from lxml import html
+
+def get_dublin_forecast():
+
+    url = 'https://www.met.ie/forecasts/dublin'
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+
+    dublin = tree \
+        .xpath('/html/body/div/div[1]/section'
+               '/div/div[1]/div[1]/p[2]/text()')[0] \
+        .replace('\n', ' ')
+
+    return dublin
+
+def send(url, forecast):
+    lambda_endpoint = url + '/' + forecast
+    requests.get(lambda_endpoint)
+
+def main(*args, **kwargs):
+    lambda_url = os.environ['LAMBDA_URL']
+    fc = get_dublin_forecast()
+    send(lambda_url, fc)
+
+if __name__=='__main__':
+    main(None)
+
